e0012491440473b572498c33c8e283f640d8f8db
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-12-06 22:21:17 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const moment = require('moment');
+const jimp = require('jimp');
+const Canvas = require('canvas');
+ const prefix = "*";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
+client.login(process.env.BOT_TOKEN);

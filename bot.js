f3db928d3bcb78f243a561a32eef9b857657433c
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-12-08 23:27:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,34 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+  if (message.content.startsWith(prefix +"avatar")) {
+if(!message.channel.guild) return;
+      var mentionned = message.mentions.users.first();
+  var client;
+    if(mentionned){
+        var client = mentionned; } else {
+        var client = message.author;
+    }
+      const embed = new Discord.RichEmbed()
+                         .addField('Requested by:', "<@" + message.author.id + ">")
+      .setColor(000000)
+      .setImage(`${client.avatarURL}`)
+    message.channel.sendEmbed(embed);
+  }
+});
+client.on('message', message => {
+  if (message.content === "#support") {
+  let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username)
+.setColor("#9B59B6")
+.addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/NgdvJy3**")
+  
+  
+message.channel.sendEmbed(embed);
+ }
+});
+
+client.on('message', message => {
          if(message.content === prefix + "closeroom") {
                              if(!message.channel.guild) return message.reply('** This command only for servers**');
   

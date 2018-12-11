cb616dafba83c38b2eab023ba7c4d0030768dac9
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-12-11 12:46:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,23 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('+ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setThumbnail('https://cdn.discordapp.com/avatars/368141321547808768/c42716e13cb850f9ad0930af699472d0.png?size=2048nk')
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms")
+                        .addField('**WebSocket:**',api + " ms")
+         message.channel.send({embed:embed});
+                        }
+});
+
+client.on('message', message => {
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
